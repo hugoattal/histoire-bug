2ec9a367fa53fa4ea0d0de392ae528306580605d
Hugo <hugoattal@hotmail.fr>
2022-05-03 16:45:31 +0200

    first commit

---
+++ src/components/HelloWorld.story.vue
@@ -0,0 +1,12 @@
+<template>
+    <Story>
+        <HelloWorld />
+    </Story>
+</template>
+
+<script setup lang="ts">
+import HelloWorld from "./HelloWorld.vue";
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+/// <reference types="histoire" />
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [vue()],
+    histoire: {
+        setupFile: "/src/setup.ts"
+    }
+});
